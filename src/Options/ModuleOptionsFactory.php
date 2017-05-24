36757121527495b9541b3a7b6acdc83ea49a8ba4
Vasily Belosludcev <bupy765@gmail.com>
2017-05-25 02:30:47 +0500

    Update ModuleOptionsFactory.php

--- src/Options/ModuleOptionsFactory.php
+++ src/Options/ModuleOptionsFactory.php
@@ -5,6 +5,9 @@ namespace Bupy7\Mailgun\Options;
 use Interop\Container\ContainerInterface;
 use Zend\ServiceManager\Factory\FactoryInterface;
 
+/**
+ * @author Vasily Belosludcev <https://github.com/bupy7>
+ */
 class ModuleOptionsFactory implements FactoryInterface
 {
     public function __invoke(ContainerInterface $container, $requestedName, array $options = null)
